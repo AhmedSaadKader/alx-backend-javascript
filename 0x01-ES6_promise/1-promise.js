e2813a442e93d8a4b4618b0394b61c8679b9cb3e
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-07-25 15:54:50 +0300

    1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,14 @@
 function getFullResponseFromAPI(success) {
-new Promise((resolve, reject) => {
-	if (success == true) {
-		resolve()
-	}
-})
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
 }
 
 export default getFullResponseFromAPI;
