75a55e843263369f6881a14401eeb4387038c045
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-07-25 19:21:27 +0300

    9-try.js

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+// 9-try.js
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(error.toString());
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
+}
