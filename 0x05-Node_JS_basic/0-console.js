a94dca42195d72ba11a82586b8a05e77cf9a9617
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-09-22 14:31:16 +0300

    .\0-console.js

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,9 @@
-const displayMessage = (text) => {
-  process.stdout.write(text);
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
 };
 
 module.exports = displayMessage;
