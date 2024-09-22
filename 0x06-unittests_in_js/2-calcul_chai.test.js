d698c0d074ed1cb33c2d779b79a7f86c97bcea16
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-09-22 19:12:11 +0300

    .\2-calcul_chai.js

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -10,25 +10,25 @@ describe('it performs calculations on the rounded numbers', () => {
       expect(calculateNumber('SUM', 1.0, 2.0)).to.equal(3);
     });
     it('correctly rounds number', () => {
-      assert.equal(calculateNumber('SUM', 1.2, 2.4), 3);
+      expect(calculateNumber('SUM', 1.2, 2.4)).to.equal(3);
     });
   });
   describe('returns subtraction if type is subtract', () => {
     it('should return 2', () => {
-      assert.equal(calculateNumber('SUBTRACT', 3, 1), 2);
+      expect(calculateNumber('SUBTRACT', 3, 1)).to.equal(2);
     });
   });
   describe('returns division if type is DIVIDE', () => {
     it('should return 3', () => {
-      assert.equal(calculateNumber('DIVIDE', 3.1, 1), 3);
+      expect(calculateNumber('DIVIDE', 3.1, 1)).to.equal(3);
     });
     it('should return Error', () => {
-      assert.equal(calculateNumber('DIVIDE', 3.1, 0), 'Error');
+      expect(calculateNumber('DIVIDE', 3.1, 0)).to.equal('Error');
     });
   });
   describe('returns NAN if type is incorrect', () => {
     it('should return 3', () => {
-      assert.equal(calculateNumber('error', 3.4, 1), undefined);
+      expect(calculateNumber('error', 3.4, 1)).to.equal(undefined);
     });
   });
 });
