2fd379bbbfae6adbf923fef5d975475874dfe44f
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-09-22 14:48:22 +0300

    .\1-stdin.js

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,16 @@
+/**
+ *
+ */
+
+const displayMessageWithName = () => {
+  console.log('Welcome to Holberton School, what is your name?');
+  process.stdin.on('readable', function () {
+    const chunk = process.stdin.read();
+    if (chunk !== null) {
+      process.stdout.write(`Your name is: ${chunk}`);
+      console.log('This important software is now closing');
+    }
+  });
+};
+
+displayMessageWithName();
